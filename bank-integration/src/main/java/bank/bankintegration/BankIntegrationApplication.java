56406471377000227bfb43e8622a7850f9440afc
Luis Acuña <luisandresverdugox@gmail.com>
2023-08-20 14:51:07 -0500

    Subiendo estructura del proyecto

---
+++ bank-integration/src/main/java/bank/bankintegration/BankIntegrationApplication.java
@@ -0,0 +1,13 @@
+package bank.bankintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankIntegrationApplication.class, args);
+	}
+
+}
---
+++ bank-integration/src/test/java/bank/bankintegration/BankIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.bankintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
